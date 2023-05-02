79e3d738764bd84b74dea2500b96b1845655f0d8
Antonio <antoniopsilva083@gmail.com>
2023-05-02 00:49:03 -0300

    initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter } from "react-router-dom"
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/theme/defaultTheme"
+import { Router } from "./Route"
+import { GlobalStyles } from "./styles/global"
+
+export function App() {
+  return (
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyles />
+
+        <Router />
+      </ThemeProvider>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,5 @@
+export function Home() {
+  return (
+    <div>Working Homepage</div>
+  )
+}
